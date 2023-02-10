06930f846c732be30284141e03ef1af12d44ad5b
Matteus Westman <matteus.westman@surikat.se>
2023-02-10 10:57:40 +0100

    revert: revert test

--- src/createSlfSentryDriver.spec.ts
+++ src/createSlfSentryDriver.spec.ts
@@ -1,24 +1,7 @@
-import createSlfSentryDriver from './createSlfSentryDriver';
-
 describe('#createSlfSentryDriver', () => {
   it.todo('should send exception to Sentry if there is an error in the parameters');
   it.todo('should send message to Sentry if there are no errors in the parameters');
   it.todo('should provide context parameters to Sentry if provided in the event');
   it.todo('should not send to Sentry if the log level is below specified error level');
   it.todo('should not send to Sentry if the log level is not found in error levels');
-  it('should be possible to change a integration option', () => {
-    const sentryUrl = 'https://sentry.io';
-    const logger = createSlfSentryDriver(sentryUrl, {
-      integrationsOptions: {
-        OnUncaughtException: {
-          onFatalError() {}
-        },
-        Http: {
-          tracing: true
-        }
-      }
-    });
-
-    expect(logger).toBeDefined();
-  });
 });
