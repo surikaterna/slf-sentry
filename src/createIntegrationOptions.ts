26749c2b7fae138af881e2176f7390b1ea3580fb
Matteus Westman <matteus.westman@surikat.se>
2023-02-14 10:36:53 +0100

    feat: new method to create integrations

---
+++ src/createIntegrationOptions.ts
@@ -0,0 +1,24 @@
+import { Integrations } from '@sentry/node';
+import type {} from '@sentry/node/types';
+import type { Integration, IntegrationClass } from '@sentry/types';
+import type { IntegrationsOptions } from './createSlfSentryDriver';
+
+const createIntegrationOptions = (options?: Partial<IntegrationsOptions>) => {
+  if (!options) {
+    return [];
+  }
+
+  const overridesIntegrationsOptions: Array<Integration> = [];
+
+  Object.entries(options).forEach(([key, value]) => {
+    const NewIntegration = Integrations[key as keyof typeof Integrations] as IntegrationClass<Integration>;
+
+    if (NewIntegration) {
+      overridesIntegrationsOptions.push(new NewIntegration(value));
+    }
+  });
+
+  return overridesIntegrationsOptions;
+};
+
+export default createIntegrationOptions;
